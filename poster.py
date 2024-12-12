0fcb2cf009705e98b0869f32d6960d00bf7a3254
pyeongan-213 <pa98213@naver.com>
2024-12-12 10:09:57 +0900

    Add files via upload

---
+++ poster.py
@@ -0,0 +1,38 @@
+from openai import OpenAI
+import streamlit as st
+
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+st.title('🎁 제품 홍보 포스터 생성기')
+
+keyword = st.text_input("키워드를 입력하세요.")
+
+if st.button("생성하기🔥"):
+    with st.spinner('생성 중입니다.'):
+        response = client.chat.completions.create(
+            model='gpt-3.5-turbo',
+            messages=[
+                {
+                    "role": "system",
+                    "content": "입력 받은 키워드에 대한 150자 이내의 솔깃한 제품 홍보 문구를 알려줘."
+                },
+                {
+                    "role": "user",
+                    "content": keyword
+                }
+            ]
+        )
+
+        st.success(response.choices[0].message.content)
+
+        image_response = client.images.generate(
+            model="dall-e-3",
+            prompt = keyword,
+            size = '1024x1024',
+            n=1
+        )
+
+        image_url = image_response.data[0].url
+        st.image(image_url)
